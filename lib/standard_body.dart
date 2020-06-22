b34957e4d84bfde521137ceb0dead4d1daa05d4e
꽃똥이 <lhs9403@naver.com>
2020-06-23 01:25:14 +0900

    Simplify code about button create & Apply color and padding for layout

--- lib/standard_body.dart
+++ lib/standard_body.dart
@@ -9,19 +9,48 @@ class StandardBody extends StatefulWidget {
 class StandardBodyState extends State<StandardBody> {
 
   static final Calculator _calc = Calculator();
+  final EdgeInsets _padding = EdgeInsets.all(1.5);
+
+  Widget createBtn(String btnText, double btnWidth, double btnHeigth, Color btnColor) {
+    return Container(
+      width: btnWidth,
+      height: btnHeigth,
+      child: Padding(
+        padding: _padding,
+        child: Container(
+          color: btnColor,
+          child: MaterialButton(
+            onPressed: () {
+              setState(() {
+                _calc.func(btnText);
+              });
+            },
+            child: Text(btnText),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    double width = size.width;
-    double heigth = size.height;
+    double width = size.width - _padding.horizontal;
+    double heigth = size.height - _padding.vertical;
 
     double button_6_width = width / 6;
     double button_4_width = width / 4;
     double row_height = heigth / 11;
+    double row_height2 = row_height * 0.6;
+
+    Color backGround = Color.fromARGB(255, 183, 183, 183);
+    Color btnNumber = Color.fromARGB(255, 241, 241, 241);
+    Color btnEqual = Color.fromARGB(255, 100, 158, 196);
+    Color btnMemory = Color.fromARGB(255, 183, 183, 183);
+    Color btnOperation = Color.fromARGB(255, 211, 211, 211);
 
     return Container(
-        color: Color.fromARGB(100, 184, 184, 184),
+        color: backGround,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
@@ -55,403 +84,77 @@ class StandardBodyState extends State<StandardBody> {
               ),
             ),
 
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('MC');
-                          });
-                        },
-                        child: Text('MC'),
-                      ),
-                    ),
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('MR');
-                          });
-                        },
-                        child: Text('MR'),
-                      ),
-                    ),
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('M＋');
-                          });
-                        },
-                        child: Text('M＋'),
-                      ),
-                    ),
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('M－');
-                          });
-                        },
-                        child: Text('M－'),
-                      ),
-                    ),
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('MS');
-                          });
-                        },
-                        child: Text('MS'),
-                      ),
-                    ),
-                    Container(
-                      width: button_6_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('Mˇ');
-                          });
-                        },
-                        child: Text('Mˇ'),
-                      ),
-                    ),
-                  ],
-                ),
+            Padding(
+              padding: _padding,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      createBtn('MC', button_6_width, row_height2, btnMemory),
+                      createBtn('MR', button_6_width, row_height2, btnMemory),
+                      createBtn('M＋', button_6_width, row_height2, btnMemory),
+                      createBtn('M－', button_6_width, row_height2, btnMemory),
+                      createBtn('MS', button_6_width, row_height2, btnMemory),
+                      createBtn('Mˇ', button_6_width, row_height2, btnMemory),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('%');
-                          });
-                        },
-                        child: Text('%'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('CE');
-                          });
-                        },
-                        child: Text('CE'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('C');
-                          });
-                        },
-                        child: Text('C'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('Back');
-                          });
-                        },
-                        child: Icon(Icons.backspace),
-                      ),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      createBtn('%', button_4_width, row_height, btnOperation),
+                      createBtn('CE', button_4_width, row_height, btnOperation),
+                      createBtn('C', button_4_width, row_height, btnOperation),
+                      createBtn('Back', button_4_width, row_height, btnOperation),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('1/x');
-                          });
-                        },
-                        child: Text('1/x'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('x²');
-                          });
-                        },
-                        child: Text('x²'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('²√x');
-                          });
-                        },
-                        child: Text('²√x'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('÷');
-                          });
-                        },
-                        child: Text('÷'),
-                      ),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      createBtn('1/x', button_4_width, row_height, btnOperation),
+                      createBtn('x²', button_4_width, row_height, btnOperation),
+                      createBtn('²√x', button_4_width, row_height, btnOperation),
+                      createBtn('÷', button_4_width, row_height, btnOperation),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('7');
-                          });
-                        },
-                        child: Text('7'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('8');
-                          });
-                        },
-                        child: Text('8'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('9');
-                          });
-                        },
-                        child: Text('9'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('×');
-                          });
-                        },
-                        child: Text('×'),
-                      ),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      createBtn('7', button_4_width, row_height, btnNumber),
+                      createBtn('8', button_4_width, row_height, btnNumber),
+                      createBtn('9', button_4_width, row_height, btnNumber),
+                      createBtn('×', button_4_width, row_height, btnOperation),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('4');
-                          });
-                        },
-                        child: Text('4'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('5');
-                          });
-                        },
-                        child: Text('5'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('6');
-                          });
-                        },
-                        child: Text('6'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('-');
-                          });
-                        },
-                        child: Text('-'),
-                      ),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      createBtn('4', button_4_width, row_height, btnNumber),
+                      createBtn('5', button_4_width, row_height, btnNumber),
+                      createBtn('6', button_4_width, row_height, btnNumber),
+                      createBtn('-', button_4_width, row_height, btnOperation),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('1');
-                          });
-                        },
-                        child: Text('1'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('2');
-                          });
-                        },
-                        child: Text('2'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('3');
-                          });
-                        },
-                        child: Text('3'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('+');
-                          });
-                        },
-                        child: Text('+'),
-                      ),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      createBtn('1', button_4_width, row_height, btnNumber),
+                      createBtn('2', button_4_width, row_height, btnNumber),
+                      createBtn('3', button_4_width, row_height, btnNumber),
+                      createBtn('+', button_4_width, row_height, btnOperation),
+                    ],
+                  ),
 
-                Row(
-                  children: [
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('＋/－');
-                          });
-                        },
-                        child: Text('＋/－'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('0');
-                          });
-                        },
-                        child: Text('0'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('.');
-                          });
-                        },
-                        child: Text('.'),
-                      ),
-                    ),
-                    Container(
-                      width: button_4_width,
-                      height: row_height,
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            _calc.func('=');
-                          });
-                        },
-                        child: Text('='),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                  Row(
+                    children: [
+                      createBtn('＋/－', button_4_width, row_height, btnNumber),
+                      createBtn('0', button_4_width, row_height, btnNumber),
+                      createBtn('.', button_4_width, row_height, btnNumber),
+                      createBtn('=', button_4_width, row_height, btnEqual),
+                    ],
+                  ),
+
+                ],
+              ),
             ),
           ],
         )
